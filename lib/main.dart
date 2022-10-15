a134fee55fa4d63cd4e9e642abd3db304403ab7c
Aleexeev Semyon <isip_s.a.alekseev@mpt>
2022-10-15 12:54:27 +0300

    add layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pr1/screen_three.dart';
+import 'package:pr1/screen_two.dart';
 import 'welcome.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Welcome(),
+      home: ScreenThree (),
     );
   }
 }
